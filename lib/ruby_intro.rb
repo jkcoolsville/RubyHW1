cc05dec0e0f3f791d581db90a70c937c0a839161
josh_kutsch <jkcoolsville@gmail.com>
2017-02-02 04:33:20 +0000

    Completed assignment

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,65 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.inject(0,:+);
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  sum(arr.sort.last(2))
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.length == 0 or arr.length == 1 then return false;
+  else
+    arr.combination(2).any? {|combo| sum(combo) == n}
+  end
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name.to_s
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s.start_with?("a", "e", "i", "o", "u", "A", "E", "I", "O", "U")
+    return false
+  elsif s.length == 0
+    return false
+  elsif /^[^\W]/.match(s) == nil
+      return false
+  else
+    return true
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if (s =~ /[^01]/ || s.length == 0)
+    return false
+  elsif (s.to_i(2) % 4 == 0)
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    if(isbn.length == 0)
+      raise ArgumentError
+    end
+    if(price <= 0)
+      raise ArgumentError
+    end
+    @isbn = isbn.to_s
+    @price = price.to_f
+  end
+  
+  attr_accessor :isbn, :price
+  
+  def price_as_string
+      format("$%.2f",@price)
+  end
 end
